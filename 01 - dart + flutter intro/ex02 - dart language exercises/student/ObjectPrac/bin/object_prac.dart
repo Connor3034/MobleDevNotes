6879fd230918933649fadfb70513763316b61d02
Connor3034 <169837659+Connor3034@users.noreply.github.com>
2026-09-13 09:41:15 -0600

    created the student class

--- 01 - dart + flutter intro/ex02 - dart language exercises/student/ObjectPrac/bin/object_prac.dart
+++ 01 - dart + flutter intro/ex02 - dart language exercises/student/ObjectPrac/bin/object_prac.dart
@@ -1,5 +1,7 @@
-import 'package:object_prac/student.dart' as object_prac;
+import 'dart:ffi';
+
+import 'package:object_prac/student.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${object_prac.calculate()}!');
+  
 }
--- 01 - dart + flutter intro/ex02 - dart language exercises/student/ObjectPrac/lib/student.dart
+++ 01 - dart + flutter intro/ex02 - dart language exercises/student/ObjectPrac/lib/student.dart
@@ -1,9 +1,54 @@
-import 'dart:ffi';
 
 class Student {
-final String FirstName;
-final String LastName;
-final Int IsNumber;
+  // fields
+late String _firstName;
+late String _lastName;
+late int _idNumber;
 
+Student.empty();
+
+Student(String firstName, String lastName, int idNumber){
+ setFirstName(firstName);
+ setLastName(lastName);
+ setIdNumber(idNumber);
+}
+
+String getFirstName(){
+  return _firstName;
+}
+
+void setFirstName(String value){
+  if (value.trim().isEmpty){
+    throw Exception("First name cannot be empty!");
+  }
+  _firstName = value;
+}
+
+String getLastName(){
+  return _lastName;
+}
+
+void setLastName(String value){
+  if (value.trim().isEmpty){
+    throw Exception("Last name cannot be empty!");
+  }
+  _lastName = value;
+}
+
+int getIdNumber(){
+  return _idNumber;
+}
+
+int setIdNumber(int value){
+  if (value <= 0){
+    throw Exception("Id cannot be negitive!");
+  }
+  value = _idNumber;
+  return _idNumber;
+}
+
+String getFullName(){
+  return "${getLastName()}, ${getFirstName()}";
+}
 
 }
